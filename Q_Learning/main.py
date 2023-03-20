ab5f3e4e0a3fda4fa7568c7e888d70432b054b0f
Shawn Zhao <shawnzhao@ShawnMac.local>
2023-03-20 17:45:39 +0800

    20230320_MLE

--- Q_Learning/main.py
+++ Q_Learning/main.py
@@ -1,55 +1,60 @@
+"""
+A simple example for Reinforcement Learning using table lookup Q-learning method.
+An agent "o" is on the left of a 1 dimensional world, the treasure is on the rightmost location.
+Run this program and to see how the agent will improve its strategy of finding the treasure.
+View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+"""
 import numpy as np
 import pandas as pd
 import time
 
-np.random.seed(2) # reproducible  the same number
+np.random.seed(2)
 
-# env
-N_STATE = 6
+N_STATES = 6
 ACTIONS = ['left', 'right']
 EPSILON = 0.9
-ALPHA = 0.1 # learning rate
-LAMBDA = 0.9 # discount factor
-MAX_EPISODES = 13 # max episodes
-FRESH_TIME = 0.3 # fresh time for one move
+ALPHA = 0.1
+GAMMA = 0.9
+MAX_EPISODES = 13
+FRESH_TIME = 0.3
 
 def build_q_table(n_states, actions):
     table = pd.DataFrame(
         np.zeros((n_states, len(actions))),
-        columns=actions, #actions' name   columns's lable
+        columns=actions,
     )
-    print(table)
     return table
 
+
 def choose_action(state, q_table):
-    # this is how to choose an action
-    state_actions = q_table.iloc[state, :]
-    if (np.random.uniform() > EPSILON) or (state_actions.all() == 0):
+    # This is how to choose an action
+    state_actions = q_table.iloc[state,:]
+    if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):
         action_name = np.random.choice(ACTIONS)
-    else: # act greedy
-        action_name = state_actions.argmax() # choose the max one
+    else:
+        action_name = state_actions.idxmax()
     return action_name
 
 def get_env_feedback(S, A):
-    # this is how agent will interact with the environment
+    # This is how agent will interact with the environment
     if A == 'right':
-        if S == N_STATE - 2: # terminate N_STATE - 1为end，然后又向右走
+        if S == N_STATES - 2:
             S_ = 'terminal'
             R = 1
         else:
-            S_ = S + 1
             R = 0
+            S_ = S + 1
     else:
         R = 0
         if S == 0:
-            S_ = S # reach the wall
+            S_ = S
         else:
             S_ = S - 1
     return S_, R
 
 def update_env(S, episode, step_counter):
     # This is how environment be updated
-    env_list = ['-']*(N_STATE-1) + ['T']   # '---------T' our environment
+    env_list = ['-']*(N_STATES-1) + ['T']   # '---------T' our environment
     if S == 'terminal':
         interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
         print('\r{}'.format(interaction), end='')
@@ -61,35 +66,32 @@ def update_env(S, episode, step_counter):
         print('\r{}'.format(interaction), end='')
         time.sleep(FRESH_TIME)
 
+
 def rl():
-    # main part of the RL loop
-    q_table = build_q_table(N_STATE, ACTIONS)
+    # main part of RL loop
+    q_table = build_q_table(N_STATES, ACTIONS)
     for episode in range(MAX_EPISODES):
         step_counter = 0
         S = 0
-        is_terminal = False
+        is_terminated = False
         update_env(S, episode, step_counter)
-        while not is_terminal:
-
+        while not is_terminated:
             A = choose_action(S, q_table)
-            S_, R = get_env_feedback(S, A) # take action & get next state and reward
+            S_, R = get_env_feedback(S, A)
             q_predict = q_table.loc[S, A]
             if S_ != 'terminal':
-                q_target = R + LAMBDA * q_table.iloc[S_,:].max() # next state is not terminal
+                q_target = R + GAMMA * q_table.iloc[S_,:].max()
             else:
-                q_target = R # the end
-                is_terminal = True
-
-            q_table.loc[S, A] += ALPHA * (q_target - q_predict)
+                q_target = R
+                is_terminated = True
+            q_table.loc[S,A] += ALPHA * (q_target - q_predict)
             S = S_
 
             update_env(S, episode, step_counter + 1)
             step_counter += 1
     return q_table
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     q_table = rl()
     print('\r\nQ-table:\n')
     print(q_table)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,3 @@
+# 20230320 Q-Learning
+
+完成Q-Learning算法的完成解析过程，并编写程序完成Q-Learning的应用。同时似然函数这个概念
